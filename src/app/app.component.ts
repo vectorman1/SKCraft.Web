da86e4c115d58d43b114c75fa3220790e7602cc5
Boris Lachev <boris.lachevbg00@gmail.com>
2020-02-12 01:43:41 +0200

    fix navbar opacity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit, Inject, Renderer2, ElementRef, ViewChild } from '@an
 import { Router, NavigationEnd } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/add/operator/filter';
-import { DOCUMENT } from '@angular/common';
-import { LocationStrategy, PlatformLocation, Location } from '@angular/common';
+import { Location } from '@angular/common';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { RoutingService } from './utilities/routing.service';
 
 @Component({
     selector: 'app-root',
@@ -15,20 +15,35 @@ export class AppComponent implements OnInit {
     private _router: Subscription;
     @ViewChild(NavbarComponent) navbar: NavbarComponent;
 
-    constructor( private renderer : Renderer2, private router: Router, @Inject(DOCUMENT,) private document: any, private element : ElementRef, public location: Location) {}
+    constructor(
+        private renderer: Renderer2, 
+        private router: Router,
+        private element : ElementRef, 
+        public location: Location,
+        private routingService: RoutingService
+    ) 
+    { }
+
     ngOnInit() {
         var navbar : HTMLElement = this.element.nativeElement.children[0].children[0];
-        this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
-            if (window.outerWidth > 991) {
-                window.document.children[0].scrollTop = 0;
-            }else{
-                window.document.activeElement.scrollTop = 0;
-            }
-            this.navbar.sidebarClose();
+
+        this._router = this.router.events
+            .filter(event => event instanceof NavigationEnd)
+            .subscribe((event: NavigationEnd) => {
+                if (window.outerWidth > 991) {
+                    window.document.children[0].scrollTop = 0;
+                }else{
+                    window.document.activeElement.scrollTop = 0;
+                }
+
+                this.navbar.sidebarClose();
         });
+
         this.renderer.listen('window', 'scroll', (event) => {
             const number = window.scrollY;
-            if (number > 150 || window.pageYOffset > 150) {
+
+            if ((number > 150 || window.pageYOffset > 150) 
+                || this.routingService.getPath() !== '/home') {
                 // add logic
                 navbar.classList.remove('navbar-transparent');
             } else {
@@ -36,8 +51,10 @@ export class AppComponent implements OnInit {
                 navbar.classList.add('navbar-transparent');
             }
         });
+
         var ua = window.navigator.userAgent;
         var trident = ua.indexOf('Trident/');
+
         if (trident > 0) {
             // IE 11 => return version number
             var rv = ua.indexOf('rv:');
@@ -46,13 +63,13 @@ export class AppComponent implements OnInit {
         if (version) {
             var body = document.getElementsByTagName('body')[0];
             body.classList.add('ie-background');
-
         }
-
     }
+
     removeFooter() {
         var titlee = this.location.prepareExternalUrl(this.location.path());
         titlee = titlee.slice( 1 );
+
         if(titlee === 'signup' || titlee === 'nucleoicons'){
             return false;
         }

fd9dec59148d06a07bd46f612038b53111033d0a
Boris Lachev <boris.lachevbg00@gmail.com>
2020-02-12 14:36:14 +0200

    update routes and indentation

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ComponentsComponent } from './components/components.component';
-import { ProfileComponent } from './examples/profile/profile.component';
-import { SignupComponent } from './examples/signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
 import { LandingComponent } from './examples/landing/landing.component';
 import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
 import { HomeComponent } from './home/home.component';
@@ -14,32 +14,34 @@ import { TermsComponent } from './terms/terms.component';
 import { PrivacyComponent } from './privacy/privacy.component';
 import { RulesComponent } from './rules/rules.component';
 import { NewsComponent } from './news/news.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'apply', component: ApplyComponent },
-  { path: 'terms', component: TermsComponent },
-  { path: 'privacy', component: PrivacyComponent },
-  { path: 'rules', component: RulesComponent },
-  { path: 'news', component: NewsComponent },
-  { path: 'landing', component: LandingComponent },
-  { path: 'demo', component: ComponentsComponent },
-  { path: 'user-profile', component: ProfileComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'landing', component: LandingComponent },
-  { path: 'nucleoicons', component: NucleoiconsComponent }
+	{ path: '', redirectTo: 'home', pathMatch: 'full' },
+	{ path: 'home', component: HomeComponent },
+	{ path: 'apply', component: ApplyComponent },
+	{ path: 'terms', component: TermsComponent },
+	{ path: 'privacy', component: PrivacyComponent },
+	{ path: 'rules', component: RulesComponent },
+	{ path: 'news', component: NewsComponent },
+	{ path: 'profile', component: ProfileComponent },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'signup', component: SignupComponent },
+
+	{ path: 'landing', component: LandingComponent },
+	{ path: 'demo', component: ComponentsComponent },
+	{ path: 'nucleoicons', component: NucleoiconsComponent }
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    BrowserModule,
-    RouterModule.forRoot(routes, {
-      useHash: true
-    })
-  ],
-  exports: [
-  ],
+	imports: [
+		CommonModule,
+		BrowserModule,
+		RouterModule.forRoot(routes, {
+			useHash: true
+		})
+	],
+	exports: [
+	],
 })
 export class AppRoutingModule { }

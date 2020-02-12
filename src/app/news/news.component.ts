a09855f6e3bce1264f7a0adb69105a6435b67dcf
Boris Lachev <boris.lachevbg00@gmail.com>
2020-02-12 12:41:54 +0200

    remove spinner from news component

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -3,22 +3,22 @@ import { News } from './news';
 import { NewsService } from './news.service';
 
 @Component({
-  selector: 'app-news',
-  templateUrl: './news.component.html',
-  styleUrls: ['./news.component.scss']
+	selector: 'app-news',
+	templateUrl: './news.component.html',
+	styleUrls: ['./news.component.scss']
 })
 export class NewsComponent implements OnInit {
 
-  constructor(private newsService: NewsService) { }
+	constructor(private newsService: NewsService) { }
 
-  newsList: News[];
+	newsList: News[];
 
-  ngOnInit(): void {
-    this.getNews();
-  }
+	ngOnInit(): void {
+		this.getNews();
+	}
 
-  getNews(): void {
-	this.newsService.get()
-		.subscribe(news => this.newsList = news);
-  }
+	getNews(): void {
+		this.newsService.get()
+			.subscribe(news => this.newsList = news);
+	}
 }

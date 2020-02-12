ffc35fa93feab41bbfd5eab22f8df85c932adc1c
Boris Lachev <boris.lachevbg00@gmail.com>
2020-02-12 12:44:35 +0200

    update routes and indentation

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ComponentsComponent } from './components/components.component';
@@ -13,26 +13,29 @@ import { ApplyComponent } from './apply/apply.component';
 import { TermsComponent } from './terms/terms.component';
 import { PrivacyComponent } from './privacy/privacy.component';
 import { RulesComponent } from './rules/rules.component';
+import { NewsComponent } from './news/news.component';
 
-const routes: Routes =[
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    { path: 'apply', component: ApplyComponent },
-    { path: 'terms', component: TermsComponent},
-    { path: 'privacy', component: PrivacyComponent},
-    { path: 'rules', component: RulesComponent },
-    { path: 'demo',             component: ComponentsComponent },
-    { path: 'user-profile',     component: ProfileComponent },
-    { path: 'signup',           component: SignupComponent },
-    { path: 'landing',          component: LandingComponent },
-    { path: 'nucleoicons',      component: NucleoiconsComponent }
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'apply', component: ApplyComponent },
+  { path: 'terms', component: TermsComponent },
+  { path: 'privacy', component: PrivacyComponent },
+  { path: 'rules', component: RulesComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'landing', component: LandingComponent },
+  { path: 'demo', component: ComponentsComponent },
+  { path: 'user-profile', component: ProfileComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'landing', component: LandingComponent },
+  { path: 'nucleoicons', component: NucleoiconsComponent }
 ];
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule.forRoot(routes,{
+    RouterModule.forRoot(routes, {
       useHash: true
     })
   ],

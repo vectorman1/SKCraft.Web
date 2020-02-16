3f9bbe4a7281421d65f55b9d33133a8ccfe518d6
Boris Lachev <boris.lachevbg00@gmail.com>
2020-02-16 21:38:37 +0200

    update profile component

--- src/app/account/profile/profile.component.ts
+++ src/app/account/profile/profile.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/authentication/auth.service';
+import { JwtUser } from 'src/app/core/authentication/JwtUser';
+import { log } from 'util';
+import { UserData } from '../models/UserData';
+import { WebService } from '../services/web.service';
+
 
 @Component({
 	selector: 'app-profile',
 	templateUrl: './profile.component.html',
 	styleUrls: ['./profile.component.scss']
 })
-
 export class ProfileComponent implements OnInit {
 	isCollapsed = false;
+	user: JwtUser;
+	userData: UserData;
+	minecraftUuid: string = this.webSerivce.getMinecraftUuid();
 
-	constructor() { }
+	constructor(private authService: AuthService, private webSerivce: WebService) { }
 
-	ngOnInit() { }
+	ngOnInit() {
+		this.user = this.authService.getUser();
+		this.userData = this.webSerivce.getUserData();
+	}
 
 }
